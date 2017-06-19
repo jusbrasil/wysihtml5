7f846b4e7c2d62cb6623534fc08346ff48f9b256
Felipe Matos Santana <felipems@yahoo.com.br>
2017-06-19 17:27:35 -0300

    Add edit dialog listener

--- src/toolbar/toolbar.js
+++ src/toolbar/toolbar.js
@@ -85,6 +85,10 @@
           that.editor.fire("show:dialog", { command: command, dialogContainer: dialogElement, commandLink: link });
         });
 
+        dialog.on("edit", function(attributes) {
+          that.editor.fire("edit:dialog", { command: command, dialogContainer: dialogElement, commandLink: link });
+        });
+
         dialog.on("save", function(attributes) {
           if (caretBookmark) {
             that.composer.selection.setBookmark(caretBookmark);
